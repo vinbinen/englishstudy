39acdf589021bc12a61d69712b8963983d2c78b0
vinbinen <77711368+vinbinen@users.noreply.github.com>
2021-01-24 20:45:42 +0800

    Delete readcopy-old.js

--- news/readcopy-old.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('copy', function() {
-                var contents = window.getSelection().toString();
-          //alert(contents);      
-	
-		document.getElementById("readcopy").innerHTML="<audio autoplay src='http://dict.youdao.com/dictvoice?type=1&audio="+contents+"'>+"
-
-"+</audio>";
-               // alert(contents);
-})
