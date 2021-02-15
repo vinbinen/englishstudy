e6d661d9b9dd2a5b254a719a13cab8f43bfe3759
vinbinen <77711368+vinbinen@users.noreply.github.com>
2021-02-15 14:31:49 +0800

    Update translatedtext.js

--- news/translatedtext.js
+++ news/translatedtext.js
@@ -2,13 +2,13 @@ document.getElementById("btn").onclick = function () {
         
         if (this.value === "隐藏译文") {
           
-            for(var i =0;i<=8;i++){
+            for(var i =0;i<=30;i++){
             document.getElementById("dv"+i).style.display = "none";
         
             this.value = "显示译文";}
         } else if (this.value === "显示译文") {
            
-            for(var i =0;i<=8;i++){
+            for(var i =0;i<=30;i++){
             document.getElementById("dv"+i).style.display = "block";
             
             this.value = "隐藏译文";}
